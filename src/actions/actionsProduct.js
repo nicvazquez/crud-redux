bf2c06a6eb3c9321b994b754eec58f3e77cf04b3
Nicolas <vazqueznicolasdev@gmail.com>
2022-11-07 16:22:45 -0300

    Defining the first redux actions

---
+++ src/actions/actionsProduct.js
@@ -0,0 +1,1 @@
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from "../types";
--- src/reducers/reducerProducts.js
+++ src/reducers/reducerProducts.js
@@ -1,3 +1,5 @@
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from "../types";
+
 // Each reducer has its own state
 const initialState = {
 	products: [],
