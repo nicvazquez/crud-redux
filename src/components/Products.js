5e17384261a435593df89d28491bec4f6f32a853
Nicolas <vazqueznicolasdev@gmail.com>
2022-11-07 15:56:37 -0300

    Products list and edit products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export const Products = () => {
-	return <h1>From Products.js</h1>;
+	return (
+		<>
+			<h2 className="text-center my-5">Products List</h2>
+
+			<table className="table table-striped">
+				<thead className="bg-primary table-dark">
+					<tr>
+						<th scope="col">Name</th>
+						<th scope="col">Price</th>
+						<th scope="col">Actions</th>
+					</tr>
+				</thead>
+
+				<tbody></tbody>
+			</table>
+		</>
+	);
 };
